5cee1b0d1d9e66ba68f5abc2fbb597f9d5c9b623
thefomin <raveyt@yandex.ru>
2025-10-18 08:14:10 +0700

    feat: telegram provider added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./app"
+import { TelegramProvider } from "./shared/provider/telegram-provider"
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<TelegramProvider>
+			<App />
+		</TelegramProvider>
 	</StrictMode>
 )
