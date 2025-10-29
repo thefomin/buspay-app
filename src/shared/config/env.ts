563ac20da019a11e0bcd2a1014b2733738d7c271
thefomin <raveyt@yandex.ru>
2025-10-29 13:51:02 +0700

    chore: added env config

---
+++ src/shared/config/env.ts
@@ -0,0 +1,6 @@
+export const CONFIG_ENV = {
+	API_TICKET_URL: import.meta.env.VITE_API_TICKET_URL,
+	API_PAYMENT_URL: import.meta.env.VITE_API_PAYMENT_URL,
+	DEV_TG_ID: import.meta.env.VITE_API_DEV_TG_ID,
+	DEV_TG_USERNAME: import.meta.env.VITE_API_DEV_TG_USERNAME,
+}
