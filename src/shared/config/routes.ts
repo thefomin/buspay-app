42cf489e008cf9440d73e08ac6ab1c2a565790d1
thefomin <raveyt@yandex.ru>
2025-11-01 00:05:52 +0700

    chore: added pathparams

--- src/shared/config/routes.ts
+++ src/shared/config/routes.ts
@@ -3,11 +3,15 @@ import "react-router-dom"
 export const ROUTES = {
 	HOME: "/",
 	CARRIER: "/carrier/:carrierId",
+	RECEIPT: "/receipt/:receiptId",
 } as const
 
 export type PathParams = {
 	[ROUTES.CARRIER]: {
 		carrierId: string
+	}
+	[ROUTES.RECEIPT]: {
+		receiptId: string
 	}
 }
 
